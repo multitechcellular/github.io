658725d57fcd7b1bc52d0560331728ed2fd9ab24
multitechcellular <apifuloh@gmail.com>
2024-09-21 00:44:43 +0700

    changed code navbar

--- js/main.js
+++ js/main.js
@@ -100,4 +100,16 @@
     });
     
 })(jQuery);
+document.addEventListener("DOMContentLoaded", function () {
+    var navbarToggler = document.querySelector(".navbar-toggler");
+    var navbarNav = document.querySelector("#navbarCollapse");
+  
+    document.querySelectorAll("#navbarCollapse .nav-link").forEach(function (link) {
+      link.addEventListener("click", function () {
+        if (window.getComputedStyle(navbarNav).display !== "none") {
+          navbarToggler.click(); // Simulate click to close the menu
+        }
+      });
+    });
+  });
 
